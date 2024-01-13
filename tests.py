8a7c3aa11438fe85baf10ebc2253ec90d9e9d57e
Michael <mblanc@student.ethz.ch>
2024-01-13 17:48:14 +0100

    change SC labels colorbar

--- tests.py
+++ tests.py
@@ -3,12 +3,13 @@ import matplotlib.pyplot as plt
 import xarray as xr
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
-import cartopy
-import json
+#import cartopy
+#import json
 import pandas as pd
 from matplotlib.colors import TwoSlopeNorm, ListedColormap
-from matplotlib.ticker import MultipleLocator
-from Scell_tracker import label_local_maxima, label_above_threshold, find_overlaps
+#from matplotlib.ticker import MultipleLocator
+from Scell_tracker import label_above_threshold, find_overlaps
+from skimage.segmentation import expand_labels
 
 import sys
 sys.path.append("../first_visu")
@@ -16,11 +17,10 @@ from CaseStudies import IUH
 
 # ================================================================================================================================
 #inputs
-dtstr = "20210713110000" # to be filled: full date and time of timeshot
+dtstr = "20210620150000" # to be filled: full date and time of timeshot
 cut = "swisscut"
 dt = pd.to_datetime(dtstr, format="%Y%m%d%H%M%S")
 aura = 1
-
 
 # load geographic features
 resol = '10m'  # use data at this scale
@@ -38,8 +38,18 @@ iuh_max = 150 # set here the maximum (or minimum in absolute value) IUH that you
 norm_iuh = TwoSlopeNorm(vmin=-iuh_max, vcenter=0, vmax=iuh_max)
 ticks_iuh = np.arange(-iuh_max, iuh_max+1, 25)
 
-labeled = label_above_threshold(iuh, 75, 60, 3)
-labeled_disp = np.where(labeled == 0, np.nan, labeled)
+labeled = label_above_threshold(iuh, 75, 65, 3)
+labeled_disp = expand_labels(labeled, distance=aura)
+labeled_disp = np.where(labeled_disp == 0, np.nan, labeled_disp)
+
+vmin = 0.5
+ncells = int(np.max(labeled))
+vmax = ncells + 0.5
+ticks_lab = np.arange(0, ncells+1)
+levels_lab = np.linspace(vmin, vmax, ncells+1)
+cmap_lab = plt.cm.get_cmap('tab10')
+colors = [cmap_lab(i % 10) for i in range(10*int(np.ceil(ncells/10)))]
+cmap_lab = ListedColormap(colors)
 
 with xr.open_dataset(fs) as dset:
     lons = dset["lon"].values
@@ -56,10 +66,10 @@ plt.colorbar(cont_iuh, ticks=ticks_iuh, orientation='horizontal', label=r"IUH ($
 plt.title(dt.strftime("%d/%m/%Y %H:%M:%S"))
 
 ax2 = fig.add_subplot(2, 1, 2, projection=ccrs.PlateCarree())
-cont = ax2.pcolormesh(lons, lats, labeled_disp, cmap="tab20", transform=ccrs.PlateCarree())
+cont = ax2.contourf(lons, lats, labeled_disp, cmap=cmap_lab, levels=levels_lab, transform=ccrs.PlateCarree())
 ax2.add_feature(bodr, linestyle='-', edgecolor='k', alpha=1)
 ax2.add_feature(ocean, linewidth=0.2)
-plt.colorbar(cont, orientation='horizontal', label="supercells labels, aura=" + str(aura))
+plt.colorbar(cont, ticks=ticks_lab, orientation='horizontal', label="supercells labels, aura="+str(aura))
 
 # ================================================================================================================================
 ## test find_overlaps
@@ -69,14 +79,6 @@ with xr.open_dataset(maskfile) as dset:
     lons = dset.variables['lon']
     masks = dset['cell_mask'] # 3D matrix
     times = pd.to_datetime(dset['time'].values)
-vmin = -0.5
-ncells = int(np.max(masks)+1)
-vmax = (ncells - 1) + 0.5
-ticks_mask = np.arange(0, ncells)
-levels_mask = np.linspace(vmin, vmax, ncells+1)
-cmap_mask = plt.cm.get_cmap('tab20')
-colors = [cmap_mask(i % 20) for i in range(20*int(np.ceil(ncells/20)))]
-cmap_mask = ListedColormap(colors)
 
 # select hourly masks and times -> decrease temporal resolution to match the IUH one
 time_slice = times.strftime("%H%M%S") == dt.strftime('%H%M%S')
